c2ef29e2439ec87705f4e5d8cb1c7832b67e9229
JJewon <34835912+eldertree777@users.noreply.github.com>
2021-11-13 13:57:17 +0900

    이게뭐시여

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,14 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from typing import Optional
 
 from fastapi import FastAPI
-#import pandas as pd
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price : float
+    is_offer : Optional[bool] = None
+
 @app.get("/")
 def read_root():
-    return {"Hello" : "World"}
+    return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return{"item_name": item.name, "item_id": item_id}
+
+
+# %%
